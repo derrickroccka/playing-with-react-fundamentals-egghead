0254e8c3efe38e256bcf55d9d8a928c215819876
derrickroccka <derrickroccka@gmail.com>
2016-03-20 21:52:18 +0100

    Code of the 12h video
    Higher order components (replaces mixins)
    https://egghead.io/lessons/react-react-fundamentals-higher-order-components-replaces-mixins?series=react-fundamentals

--- App.js
+++ App.js
@@ -1,288 +1,96 @@
-/**
- * NOTE: I'm making a mix of all the videos, but I'll add comments to
- * be as specific as I can. Instead of creating new components for every video
- * I reuse the same component but adding the new features.
- * You will also find some txt values that are different, but the functionality
- * will remain exactly the same way
- * @author @derrickroccka
- */
+///////////////////////////////////////////////////////
+///**
+// *
+// * Higher order functions replaces Mixins (12th video)
+// * NOTE: I decided to clear the file and keep the code
+// * on separated branches since here.
+// * Things started to be messy and hard to understand
+// * since we are mixin a lot of concepts together
+// *
+// */
+///////////////////////////////////////////////////////
+//ORIGINAL CODE
+//
+//import React from 'react';
+//
+//class App extends React.Component {
+//	constructor(){
+//		super();
+//		this.update = this.update.bind(this);
+//		this.state = {val:0};
+//	}
+//	update(){
+//		this.setState({val:this.state.val+1});
+//	}
+//	componentWillMount(){
+//		console.log('will mount');
+//	}
+//	render(){
+//		return (
+//			<button onClick={this.update}>
+//				{this.props.txt} - {this.state.val}
+//			</button>
+//		);
+//	}
+//	componentDidMount(){
+//		console.log('mounted');
+//	}
+//}
+//
+//App.defaultProps = {txt:'button'};
 
-//We'll start importing React
+//MODIFIED CODE
+
+/////////////////////////////////////////////////////
+/**
+ *
+ * Higher order functions replaces Mixins (12th video)
+ * NOTE: I decided to clear the file and keep the code
+ * on separated branches since here.
+ * Things started to be messy and hard to understand
+ * since we are mixin a lot of concepts together
+ *
+ */
+/////////////////////////////////////////////////////
+
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-/**
- * THIS IS THE FIRST METHOD USED TO CREATE A COMPONENT WITH JSX
- * THIS IS NOT STATELESS
- * @author @derrickroccka
- */
-class App extends React.Component {
-	//App component constructor
+let Mixin = InnerComponent => class extends React.Component{
 	constructor(){
 		super();
-		this.state = {
-			txt: 'We are the same state property (txt)',
-			cat: 0,
-			red: 0,
-			green: 0,
-			blue: 0,
-			val: 9,
-			//added in the 11th videos
-			increasing: false
-		};
-		this.update = this.update.bind(this)
+		this.update = this.update.bind(this);
+		this.state = {val:0};
 	}
-
-	//Update is called after action on the DOM
-	update(e){
-		this.setState({
-			txt: e.target.value,
-			red: ReactDOM.findDOMNode(this.refs.red).value,
-			green: ReactDOM.findDOMNode(this.refs.green).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue).value,
-			val: this.state.val + 1
-		});
-		//added in 11th video
-		ReactDOM.render(
-			<App val={this.props.val+1} />,
-			document.getElementById('app')
-		)
+	update(){
+		this.setState({val:this.state.val+1});
 	}
 	componentWillMount(){
-		console.log('mounting');
-		/////////////////////////////////////////////////////
-		/**
-		 *
-		 * 10th video Component lifecycle mounting usage
-		 * Starts with this line
-		 *
-		 */
-			/////////////////////////////////////////////////////
-		this.setState({m:2});
-
+		console.log('will mount');
 	}
-	//This method will render our component
 	render(){
-		console.log('rendering!!!');
-		console.log('increasing!!!',this.state.increasing);
-		//stores txt prop value
-		let txt = this.props.txt;
-		//Its good to wrap elements into parentheses
-		//It is necessary to wrap jsx in a main tag (example: <div>)
-		return (
-			<div>
-				<div>
-					<h1>Hi</h1>
-					<b>Bold</b>
-					{/* A JSX comment - Pay attention: props are not state
-					 so this won't change when we are calling update
-					 This is passed when the app starts and its not modified*/}
-					<h2>{txt}</h2>
-				</div>
-				<hr/>
-				<div>
-					{/* This will call update everytime we type something */}
-					<input type="text" onChange={this.update} />
-					<h3>{this.state.txt}</h3>
-				</div>
-				{/* This does the same as the code above,
-					but using the Widget const below
-					This will modify the same state property*/}
-				<Widget txt={this.state.txt} update={this.update} />
-				<hr/>
-				{/* RGB SLIDERS USING REFS
-				 The txt changes too because txt is referenced to the value of the target
-				 in this case the input range that we are moving
-				 Take a look at the Slider class below*/}
-				<Slider ref="red" update={this.update} />
-				{this.state.red}
-				<br/>
-				<Slider ref="green" update={this.update} />
-				{this.state.green}
-				<br/>
-				<Slider ref="blue" update={this.update} />
-				{this.state.blue}
-				<br/>
-				<hr/>
-				{/* Accessing child properties (8th video) starts with this button
-					Similar to Angular transclusion*/}
-				<button> I <Heart/> React</button>
-				<hr/>
-				{/* lifecycle mounting basics (9th video)*/}
-				<button onClick={this.update} value="lol">{this.state.val}</button>
-				<hr/>
-				{/* lifecycle mount (10th video)*/}
-				<button onClick={this.update} value="lol">{this.state.val*this.state.m}</button>
-				<hr/>
-				{/* lifecycle update method (11th video)*/}
-				<button onClick={this.update} value="lol">{this.props.val}</button>
-			</div>
-		)
+		return <InnerComponent update={this.update} {...this.state} {...this.props} />
 	}
 	componentDidMount(){
 		console.log('mounted');
-		//this will call update once the component is mounted every 0.5s (10th video)
-		this.inc = setInterval(this.update, 500);
 	}
-	componentWillUnmount(){
-		console.log('bye!');
-		//clearing the interval set above (10th video)
-		//this is done to prevent the error that is shown when we unmount
-		//and call something that is unmounted
-		clearInterval(this.inc);
-	}
+};
 
-	componentWillReceiveProps(nextProps){
-		this.setState({increasing: nextProps.val > this.props.val});
-	}
-	//only updates the component if a condition is acomplished (11th video)
-	shouldComponentUpdate(nextProps, nextState){
-		return nextProps.val % 5 === 0;
-	}
-	//says if the component has been updated and the previous value of the property (11th video)
-	componentDidUpdate(prevProps, prevState){
-		console.log('prevProps', prevProps);
-	}
-}
+const Button = (props) => <button onClick={props.update}>{props.txt} - {props.val}</button>;
+const Label = (props) => <label onMouseMove={props.update}>{props.txt} - {props.val}</label>;
 
-//Types of the properties of the element that we're working at
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
+let ButtonMixed = Mixin(Button);
+let LabelMixed = Mixin(Label);
 
-//Default values for props. (If not speficied in the element)
-App.defaultProps = {
-	txt: 'Default txt',
-	val: 0
-}
-
-/////////////////////////////////////////////////////
-/**
- *
- * ALTERNATIVE WAYS TO CREATE AN ELEMENT
- *
- */
-/////////////////////////////////////////////////////
-
-/**
- * THIS IS THE SAME, BUT WITHOUT JSX
- * null will be explained later
- */
-//class App extends React.Component {
-//	render(){
-//		return React.createElement('h1', null, 'Hi')
-//	}
-//}
-
-/**
- * THIS IS THE SAME BUT SHORTER (ES6 ARROW FUNCTIONS + JSX)
- * THE POINT IS THAT THIS IS STATELESS. BE CAREFUL!!
- */
-//const App = () => <h1>Hi</h1>
-
-
-/////////////////////////////////////////////////////
-/**
- *
- * Owner Ownee (6th video) exercise related
- * A component renders another component
- * @author @derrickroccka
- */
-/////////////////////////////////////////////////////
-
-const Widget = (props) => {
-	return (
-		<div>
-			<input type="text" onChange={props.update}/>
-			<h3>{props.txt}</h3>
-		</div>
-	)
-}
-
-/////////////////////////////////////////////////////
-/**
- *
- * Usign Refs to Access (7th video) exercise related
- * A way to reference an instance of a component of
- * our React App
- * Refs won't work with stateless components
- * @author @derrickroccka
- */
-/////////////////////////////////////////////////////
-
-class Slider extends React.Component {
-	render(){
-		return (
-			<input type="range"
-				min="0"
-				max="255"
-				onChange={this.props.update}/>
-		)
-	}
-}
-
-/////////////////////////////////////////////////////
-/**
- *
- * Accessing Child Properties
- *
- */
-/////////////////////////////////////////////////////
-
-/**
- * Stateless component that returns a span with a bootstrap heart
- * @author @derrickroccka
- */
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>;
-
-/**
- * Button component that returns a button
- * Do not get confused: is Button not button
- * so be careful
- */
-class Button extends React.Component {
-	render(){
-		return <button>{this.props.children}</button>
-	}
-}
-
-/////////////////////////////////////////////////////
-/**
- *
- * Component lifecycle mounting basics (9th video)
- *
- */
-/////////////////////////////////////////////////////
-
-/**
- * This will create a wrapper that will contain the
- * App element inside. We are focused on mounting
- * and unmounting elements, so the point is that with
- * mount/unmount buttons, the app is appearing and
- * disappearing in the screen.
- *
- */
-class Wrapper extends React.Component {
-	constructor(){
-		super();
-	}
-	mount(){
-		ReactDOM.render(<App />, document.getElementById('a'));
-	}
-	unmount(){
-		ReactDOM.unmountComponentAtNode(document.getElementById('a'));
-	}
+class App extends React.Component {
 	render(){
 		return (
 			<div>
-				<button onClick={this.mount.bind(this)}>Mount</button>
-				<button onClick={this.unmount.bind(this)}>Unmount</button>
-				<div id="a"></div>
+				<ButtonMixed txt="Button" />
+				<LabelMixed txt="Label - 0" />
 			</div>
-		)
+		);
 	}
 }
-export default Wrapper
-//Exposing the App component (this was commented on the 9th video and changed by the line above
-//export default App
+
+export default App
+
